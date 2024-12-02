8ccf990aa094a9b98938c530bc076e070ac15a17
MikeHatchi22 <hatchi.mike@gmail.com>
2024-12-02 09:50:14 +0100

    finished algo 1

--- algo/1/countLetters.ts
+++ algo/1/countLetters.ts
@@ -10,8 +10,21 @@ Exemples :
 */
 
 function countLetters(givenString: string, letter: string): number {
-  // Your code here !
-  return 0;
+  let count: number = 0;
+  for (let i = 0; i < givenString.length; i++) {
+    if (givenString[i] === letter) {
+      count++;
+    }
+  }
+  return count;
 }
+
+countLetters("", "b");
+countLetters("a", "b");
+countLetters("a", "a");
+countLetters("bbb", "b");
+countLetters("abababab", "b");
+countLetters("abcba", "c");
+countLetters("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", "e");
 
 export default countLetters;
